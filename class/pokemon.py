9e4af17d27e87849b28a296a0a9964f2649efbb4
maxence.ginoux <maxence.ginoux@laplateforme.io>
2024-01-08 14:51:07 +0100

    new

---
+++ class/pokemon.py
@@ -0,0 +1,8 @@
+class Pokemon:
+    def __init__(self, nom, point_de_vie, type, niveau, puissance_attaque, defense):
+        self.nom = nom
+        self.point_de_vie = point_de_vie
+        self.type = type
+        self.niveau = niveau
+        self.puissance_attaque = puissance_attaque
+        self.defense = defense
